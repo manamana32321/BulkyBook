c56fe6b4ee7f1d898a3517c8f04ddfbfafed5e67
manamana32321 <65779663+manamana32321@users.noreply.github.com>
2022-03-17 20:19:33 +0900

    150 Cart Summary UI

--- BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
+++ BulkyBookWeb/Areas/Customer/Controllers/CartController.cs
@@ -66,6 +66,27 @@ namespace BulkyBookWeb.Areas.Customer.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult Summary()
+        {
+            //var claimsIdentity = (ClaimsIdentity)User.Identity;
+            //var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            //ShoppingCartVM = new ShoppingCartVM()
+            //{
+            //    ListCart = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value,
+            //    includeProperties: "Product")
+            //};
+            //foreach (var cart in ShoppingCartVM.ListCart)
+            //{
+            //    cart.Price = GetPriceBasedOnQuantity(cart.Count, cart.Product.Price,
+            //        cart.Product.Price50, cart.Product.Price100);
+            //    ShoppingCartVM.CartTotal += cart.Price * cart.Count;
+            //}
+            //return View(ShoppingCartVM);
+
+            return View();
+        }
+
         private double GetPriceBasedOnQuantity(double quantity, double price, double price50, double price100)
         {
             if (quantity <= 50)
